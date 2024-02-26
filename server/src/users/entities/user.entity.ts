96db7bf06db6ac32df5e0ec3acf9a96c50f960be
QuietSup <feeling_of_infinity@outlook.com>
2024-02-26 13:31:59 +0200

    feat(server): create User entity

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+  })
+  public created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+  })
+  public updated_at: Date;
+}
