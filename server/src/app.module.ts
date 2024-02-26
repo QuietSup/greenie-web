8a40f5234dedf53762c49580c316e3ad089f9da2
QuietSup <feeling_of_infinity@outlook.com>
2024-02-26 13:16:02 +0200

    chore(server): connect postgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'postgres',
+      entities: [User],
+      // synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
