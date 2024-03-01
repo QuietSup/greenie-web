51b48ce03214bbbd7f5c982e702b61a8b7b87d15
QuietSup <feeling_of_infinity@outlook.com>
2024-03-01 12:07:54 +0200

    temp(server): test jwt

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -5,12 +5,14 @@ import {
   UseGuards,
   Req,
   Request,
+  Get,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local.guard';
 import { UsersService } from 'src/users/users.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { JwtRefreshAuthGuard } from './guards/jwt-refresh.guard';
+import { JwtAccessAuthGuard } from './guards/jwt-access.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -31,5 +33,11 @@ export class AuthController {
   @Post('refresh-token')
   async refreshToken(@Request() req) {
     return this.authService.refreshToken(req.user);
+  }
+
+  @UseGuards(JwtAccessAuthGuard)
+  @Get('test')
+  async test(@Request() req: any) {
+    return req.user;
   }
 }
