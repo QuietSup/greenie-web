e97005b11ea41f1464aa1b283aca0a3012e76d88
QuietSup <feeling_of_infinity@outlook.com>
2024-03-03 10:19:53 +0200

    fix: use secret from config ini passport strategy

--- server/src/auth/strategies/jwt-refresh.strategy.ts
+++ server/src/auth/strategies/jwt-refresh.strategy.ts
@@ -1,15 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { authConfig } from 'src/config-namespaces/auth/auth.config';
 
 export class JwtRefreshStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
-  constructor() {
+  constructor(
+    @Inject(authConfig.KEY)
+    private readonly authConf: ConfigType<typeof authConfig>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: 'ToBeChanged',
+      secretOrKey: authConf.jwtRefreshSecret,
     });
   }
 
