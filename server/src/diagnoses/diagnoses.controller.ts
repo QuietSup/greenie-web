d525316a53ba29775180a7346c40cea6c83c6f97
QuietSup <feeling_of_infinity@outlook.com>
2024-03-04 12:01:30 +0200

    feat: add auth control to diagnoses crud

--- server/src/diagnoses/diagnoses.controller.ts
+++ server/src/diagnoses/diagnoses.controller.ts
@@ -14,6 +14,9 @@ import { UpdateDiagnosisDto } from './dto/update-diagnosis.dto';
 import { JwtAccessAuthGuard } from 'src/auth/guards/jwt-access.guard';
 import { RequestUser } from 'src/users/entities/request-user.entity';
 import { User } from 'src/auth/decorators/user.decorator';
+import { RolesGuard } from 'src/auth/modules/roles/roles.guard';
+import { Roles } from 'src/auth/modules/roles/roles.decorator';
+import { Role } from 'src/auth/modules/roles/roles.enum';
 
 @Controller('diagnoses')
 export class DiagnosesController {
@@ -25,6 +28,8 @@ export class DiagnosesController {
    * @param {RequestUser} reqUser - The authenticated user object.
    * @returns {Promise<Diagnosis[]>} An array of diagnoses associated with the user.
    */
+  @UseGuards(RolesGuard)
+  @Roles(Role.User)
   @UseGuards(JwtAccessAuthGuard)
   @Get('me')
   findAllByUser(@User() reqUser: RequestUser) {
@@ -38,6 +43,8 @@ export class DiagnosesController {
    * @param {CreateDiagnosisDto} createDiagnosisDto - The DTO containing the details of the diagnosis to be created.
    * @returns {Promise<Diagnosis>} The newly created diagnosis.
    */
+  @UseGuards(RolesGuard)
+  @Roles(Role.User)
   @UseGuards(JwtAccessAuthGuard)
   @Post()
   create(
@@ -58,6 +65,9 @@ export class DiagnosesController {
     return this.diagnosesService.findOne(+id);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(Role.User)
+  @UseGuards(JwtAccessAuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -66,6 +76,9 @@ export class DiagnosesController {
     return this.diagnosesService.update(+id, updateDiagnosisDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(Role.User)
+  @UseGuards(JwtAccessAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.diagnosesService.remove(+id);
